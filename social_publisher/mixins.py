dd4b700192c0b075f4de6edd343df9d027814e7c
Vlasov Dmitriy <scailer@veles.biz>
2015-01-16 11:07:46 +0500

    hotfix

--- social_publisher/mixins.py
+++ social_publisher/mixins.py
@@ -9,22 +9,27 @@ _core = core.PublisherCore()
 class Catcher(object):
     SocialUserDoesNotExist = core.SocialUserDoesNotExist
 
-    def __init__(self, user, comment):
+    def __init__(self, user):
         self.user = user
-        self.comment = comment
 
     def __getattr__(self, name):
         def _publish(*args, **kw):
             obj = kw.get('obj', args[0])
-            _core.publish(self.user, name, obj, comment=self.comment, **kw)
+
+            try:
+                comment = kw.get('comment', args[1])
+            except IndexError:
+                comment = None
+
+            _core.publish(self.user, name, obj, comment, **kw)
 
         return misc._safe_call(_publish)
 
 
 class PublisherForUserMixin(object):
     @property
-    def publish(self, comment=None):
-        return Catcher(user=self, comment=comment)
+    def publish(self):
+        return Catcher(user=self)
 
     def publish_to(self, provider, obj, **kw):
         return _core.publish(self, provider, obj, **kw)
