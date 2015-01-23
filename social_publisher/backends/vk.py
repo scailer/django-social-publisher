1a5c2ffdf6d386e8810635efc97010cef75cc0d7
Vlasov Dmitriy <scailer@veles.biz>
2015-01-23 10:51:32 +0500

    add simple vk to wall backend

--- social_publisher/backends/vk.py
+++ social_publisher/backends/vk.py
@@ -6,13 +6,34 @@ from social_publisher import vk
 from . import base
 
 
-class VKImageToWallBackend(base.BaseBackend):
-    name = 'vk_image_to_wall'
+class VKBackend(base.BaseBackend):
+    name = 'vk'
     auth_provider = 'vk-oauth'
 
     def get_api(self, social_user):
         return vk.API(access_token=social_user.extra_data.get('access_token'),
                       app_id=settings.VK_APP_ID, scope=['photos', 'wall'])
+
+    def get_api_publisher(self, social_user):
+        """
+            owner_id - VK user or group
+            from_group - 1 by group, 0 by user
+            message - text
+            attachments - comma separated links or VK resources ID's
+            and other https://vk.com/dev.php?method=wall.post
+        """
+
+        def _post(**kwargs):
+            api = self.get_api(social_user)
+            response = api.wall.post(**kwargs)
+            return response
+
+        return _post
+
+
+class VKImageToWallBackend(VKBackend):
+    name = 'vk_image_to_wall'
+    auth_provider = 'vk-oauth'
 
     def get_api_publisher(self, social_user):
         """
