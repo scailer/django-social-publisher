4e026864dda17bc7521df2ad129a3908af00bab3
Vlasov Dmitriy <scailer@veles.biz>
2015-01-30 13:42:54 +0500

    add scope in vk auth

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ For writing you own haandlers, see examples in source code.
 
 setup(
     name='django-social-publisher',
-    version='0.1.7',
+    version='0.1.9',
     author='Dmitriy Vlasov',
     author_email='scailer@russia.ru',
 
--- social_publisher/auth/vk.py
+++ social_publisher/auth/vk.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.conf import settings
 from social_auth.backends.contrib.vk import VKOAuth2Backend, VKOAuth2
 
 
@@ -9,6 +10,14 @@ class VKStandaloneBackend(VKOAuth2Backend):
 
 class VKStandaloneAuth(VKOAuth2):
     name = 'vk-standalone'
+    AUTH_BACKEND = VKStandaloneBackend
+
+    def get_scope(self):
+        scope = super(VKStandaloneAuth, self).get_scope()
+        scope = list(scope) if scope else []
+        extra = getattr(settings, 'VK_STANDALONE_EXTRA_SCOPE', [])
+        scope.extend(extra)
+        return scope
 
     def auth_complete(self, *args, **kwargs):
         request = kwargs.get('request')
