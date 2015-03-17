809c51d7abc50791f6611a9b6bd2a914208a4c2b
Vlasov Dmitriy <scailer@veles.biz>
2015-03-17 16:24:31 +0500

    hotfix

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ For writing you own haandlers, see examples in source code.
 
 setup(
     name='django-social-publisher',
-    version='0.1.16',
+    version='0.1.17',
     author='Dmitriy Vlasov',
     author_email='scailer@russia.ru',
 
