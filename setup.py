e42a3a3fec207b575d38bcd19c3c1791100abb2c
Vlasov Dmitriy <scailer@veles.biz>
2015-10-27 14:21:14 +0500

    bump version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ PY3 = sys.version_info[0] == 3
 
 setup(
     name='django-social-publisher',
-    version='0.4.0',
+    version='0.4.1',
     author='Dmitriy Vlasov',
     author_email='scailer@russia.ru',
 
