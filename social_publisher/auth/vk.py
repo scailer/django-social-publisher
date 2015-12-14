b780b6a22b6cdd9257738632769a5b2d6115ab99
Vlasov Dmitriy <scailer@veles.biz>
2015-12-14 13:34:05 +0500

    fix VK-standalone auth backend

--- social_publisher/auth/vk.py
+++ social_publisher/auth/vk.py
@@ -2,34 +2,13 @@
 
 from __future__ import unicode_literals
 
-from django.conf import settings
-from social_auth.backends.contrib.vk import VKOAuth2Backend, VKOAuth2
+from social.backends.vk import VKOAuth2
 
 
-class VKStandaloneBackend(VKOAuth2Backend):
+class VKStandalone(VKOAuth2):
     name = 'vk-standalone'
-
-
-class VKStandaloneAuth(VKOAuth2):
-    name = 'vk-standalone'
-    AUTH_BACKEND = VKStandaloneBackend
-    SETTINGS_KEY_NAME = 'VK_STANDALONE_APP_ID'
-    SETTINGS_SECRET_NAME = 'VK_STANDALONE_API_SECRET'
-
-    def get_scope(self):
-        scope = super(VKStandaloneAuth, self).get_scope()
-        scope = list(scope) if scope else []
-        extra = getattr(settings, 'VK_STANDALONE_EXTRA_SCOPE', [])
-        scope.extend(extra)
-        return scope
 
     def auth_complete(self, *args, **kwargs):
-        request = kwargs.get('request')
-        params = request.GET.dict()
-        return self.do_auth(params['access_token'], response=params,
-                            *args, **kwargs)
-
-
-BACKENDS = {
-    'vk-standalone': VKStandaloneAuth,
-}
+        data = self.strategy.request_data()
+        return self.do_auth(
+            data['access_token'], response=data, *args, **kwargs)
