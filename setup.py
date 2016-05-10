7fc0ea28fc9e4ecf0e95617fc2d1f89a90fca087
Vlasov Dmitriy <scailer@veles.biz>
2016-05-10 11:35:41 +0500

    up

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ PY3 = sys.version_info[0] == 3
 
 setup(
     name='django-social-publisher',
-    version='0.5.5',
+    version='0.5.6',
     author='Dmitriy Vlasov',
     author_email='scailer@veles.biz',
 
